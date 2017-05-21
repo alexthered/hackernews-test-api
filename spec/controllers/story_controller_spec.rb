07b3eaea2d23e1e8cf01b09c0a24407bb4b3504c
Hiep Doan <hiep.dv179@gmail.com>
2017-05-21 12:45:37 +0200

    temporarily disable the StoryController RSpec test as it's too time consuming

--- spec/controllers/story_controller_spec.rb
+++ spec/controllers/story_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe StoryController, type: :controller do
+RSpec.describe V1::StoryController, type: :controller do
+  # describe "GET index" do
+  #   it 'get top 10 stories' do
+  #     get :index
+  #     expect(response).to have_http_status(200)
+  #     expect(JSON.parse(response.body).size).to eql(10)
+  #   end
+  # end
 
 end
